321ce48504072a9397e281921bf4e85735af7a3f
Payel Bhattacharya <payelbhattacharya2492001@gmail.com>
2022-02-22 22:55:16 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,14 +6,14 @@ export default function Navbar(props) {
     return (
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">{props.title}</a>
+                <a className="navbar-brand" href="#">{props.title}</a>
                 <buthrefn className="navbar-hrefggler" type="buthrefn" data-bs-hrefggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="hrefggle navigation">
                     <span className="navbar-hrefggler-icon"></span>
                 </buthrefn>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auhref mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-a active" aria-current="page" href="/">Home</a>
+                            <a className="nav-a active" aria-current="page" href="#">Home</a>
                         </li>
                         {/* <li className="nav-item">
                             <a className="nav-a active mx-3" aria-current="page" href="#">{props.aboutText}</a>
